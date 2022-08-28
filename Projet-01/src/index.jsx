cd457edc47a1f914200892aaf53c01262ba643a5
Raphaël Fillion <rfillion@blanko.ca>
2022-08-28 17:07:36 -0400

    correction react 18

--- Projet-01/src/index.jsx
+++ Projet-01/src/index.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 
-createRoot(document.querySelector('#root')).render(<h1>Projet 01</h1>); // Nouvelle facon react 18
+createRoot(document.querySelector('#root')).render(<h1>Projet 01</h1>); // Nouvelle façon react 18
