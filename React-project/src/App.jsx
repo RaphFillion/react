ac84d1659b46f813eb53f67de16d778abd3eb0ab
Raphaël Fillion <rfillion@blanko.ca>
2022-08-28 13:38:19 -0400

    Premier cours react

---
+++ React-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+function App() {
+  return (
+  <>
+    <h1>Coucou</h1>
+    <p>Mon prénom est Raphaël</p>
+  </>
+  )
+}
+
+/* class App extends Component {
+  render() {
+    return <h1>Hello from another file</h1>
+  }
+} */
+
+export default App;
